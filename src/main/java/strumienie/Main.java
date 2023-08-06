3f981a7b8a00f6e7cf16b02771f33e7507652a03
Raulek123 <wywial.krzysztof@wp.pl>
2023-08-06 17:42:23 +0200

    Nauka stream.

--- src/main/java/strumienie/Main.java
+++ src/main/java/strumienie/Main.java
@@ -1,5 +1,6 @@
 package strumienie;
 
+import java.util.function.IntUnaryOperator;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -8,9 +9,20 @@ public class Main {
        IntStream stream = IntStream.of(1, 2, 3, 4, 5);
        stream.filter(n -> n % 2 != 0)
                .forEach(System.out::println);
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
         Stream<String> stringStream = Stream.of("a", "b", "c");
         stringStream.map(String::toUpperCase)
                 .forEach(System.out::println);
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+        IntStream iterate = IntStream.iterate(0, x -> x + 1);
+        iterate.limit(10)
+                .forEach(System.out::println);
+
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+        IntStream rangeClosed = IntStream.rangeClosed(0, 9);
+        rangeClosed.forEach(System.out::println);
     }
 }
