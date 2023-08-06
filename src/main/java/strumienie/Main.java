eff929c29a0e13fe10fc8fd9e946f89fcff7ea09
Raulek123 <wywial.krzysztof@wp.pl>
2023-08-06 17:34:51 +0200

    Nauka stream.

--- src/main/java/strumienie/Main.java
+++ src/main/java/strumienie/Main.java
@@ -1,11 +1,16 @@
 package strumienie;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
        IntStream stream = IntStream.of(1, 2, 3, 4, 5);
-       stream.filter(n -> n % 2 == 0)
+       stream.filter(n -> n % 2 != 0)
                .forEach(System.out::println);
+
+        Stream<String> stringStream = Stream.of("a", "b", "c");
+        stringStream.map(String::toUpperCase)
+                .forEach(System.out::println);
     }
 }
