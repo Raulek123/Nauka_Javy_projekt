c1999d7a66e9695a5415f32609169a5025623aa9
Raulek123 <wywial.krzysztof@wp.pl>
2023-08-07 17:25:08 +0200

    Dodanie klasy TerminalOperations.

---
+++ src/main/java/strumienie/TerminalOperations.java
@@ -0,0 +1,14 @@
+package strumienie;
+
+import java.util.stream.Stream;
+
+public class TerminalOperations {
+    public static void main(String[] args) {
+        Stream<Course> courses = Stream.of(
+                new Course(1L, "Java", 199, "Programowanie"),
+                new Course(2L, "Sztuka pisania", 99, "Rozwój osobisty"),
+                new Course(3L, "Tajniki Google", 299, "Marketing")
+        );
+        courses.forEach(System.out::println);
+    }
+}
