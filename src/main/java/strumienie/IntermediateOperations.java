b760d5260f83ebff85d92f802519a4f67d961042
Raulek123 <wywial.krzysztof@wp.pl>
2023-08-11 09:23:41 +0200

    put code that worked where the code that didn't used to be

--- src/main/java/strumienie/IntermediateOperations.java
+++ src/main/java/strumienie/IntermediateOperations.java
@@ -1,18 +1,22 @@
 package strumienie;
 
+import java.util.Arrays;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class IntermediateOperations {
     public static void main(String[] args) {
-        Stream<Course> courses = Stream.of(
-                new Course(1L, "Java", 249, "Programowanie"),
-                new Course(2L, "Sztuka pisania", 99, "Rozwój osobisty"),
-                new Course(3L, "Tajniki Google", 299, "Marketing"),
-                new Course(4L, "Zarządzanie zespołem", 159, "Biznes"),
-                new Course(5L, "Python od podstaw", 169, "Programowanie")
-        );
 
-        Stream<Course> expensiveCourses = courses.filter(course -> course.getPrice() > 200);
-        expensiveCourses.forEach(System.out::println);
+        Course[] cheapCourses = {
+                new Course(1L, "Java", 49, "Programowanie"),
+                new Course(2L, "Sztuka pisania", 79, "Rozwój osobisty"),
+        };
+        Course[] expensiveCourses = {
+                new Course(3L, "Tajniki Google", 299, "Marketing"),
+                new Course(4L, "Python od podstaw", 169, "Programowanie")
+        };
+        Stream.of(cheapCourses, expensiveCourses)
+                .flatMap(Arrays::stream)
+                .forEach(System.out::println);
     }
 }
