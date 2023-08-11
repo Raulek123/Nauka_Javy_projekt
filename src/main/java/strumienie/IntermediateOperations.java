fdcb31fd8685eb77db821fe751fd40560e750566
Raulek123 <wywial.krzysztof@wp.pl>
2023-08-11 08:29:17 +0200

    Zamiana strumienia typu prostego int na kolekcję i wyświetlenie kolekcji Integer.

--- src/main/java/strumienie/IntermediateOperations.java
+++ src/main/java/strumienie/IntermediateOperations.java
@@ -1,12 +1,14 @@
 package strumienie;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class IntermediateOperations {
     public static void main(String[] args) {
-        IntStream.rangeClosed(0, 10)
+        List<Integer> collect = IntStream.rangeClosed(0, 10)
                 .boxed()
                 .collect(Collectors.toList());
+        System.out.println(collect);
     }
 }
