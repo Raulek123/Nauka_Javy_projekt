a3a98122f57133a4a58252e271bb79f1d621dce4
Raulek123 <wywial.krzysztof@wp.pl>
2023-08-11 12:01:53 +0200

    added code

--- src/main/java/strumienie/IntermediateOperations.java
+++ src/main/java/strumienie/IntermediateOperations.java
@@ -7,16 +7,12 @@ public class IntermediateOperations {
     public static void main(String[] args) {
 
         Stream<Course> courses = Stream.of(
-                new Course(1L, "Java", 249, "Programowanie"),
+                new Course(1L, "Java", 149, "Programowanie"),
                 new Course(2L, "Sztuka pisania", 99, "Rozwój osobisty"),
-                new Course(1L, "Java", 249, "Programowanie"),
                 new Course(3L, "Tajniki Google", 299, "Marketing"),
                 new Course(4L, "Java od podstaw", 249, "Programowanie")
         );
-        courses.map(Course::getName)
-                .map(String::toLowerCase)
-                .filter(name -> name.contains("java"))
-                .collect(Collectors.toList())
+        courses.sorted((c1, c2) -> Double.compare(c1.getPrice(),c2.getPrice()))
                 .forEach(System.out::println);
     }
 }
