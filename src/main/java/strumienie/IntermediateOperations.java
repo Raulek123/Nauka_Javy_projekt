65be238aaf0dc99c63b8d72e227a1cb7aaf581a3
Raulek123 <wywial.krzysztof@wp.pl>
2023-08-11 08:28:32 +0200

    Metody pośrednie.

--- src/main/java/strumienie/IntermediateOperations.java
+++ src/main/java/strumienie/IntermediateOperations.java
@@ -1,7 +1,12 @@
 package strumienie;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class IntermediateOperations {
     public static void main(String[] args) {
-
+        IntStream.rangeClosed(0, 10)
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
